244a691b22eb8064f4a9832440e4dc0a92232d09
JHKIMS <wjdgns5131@gmail.com>
2023-01-14 13:54:53 +0900

    ReactJunior: Ts을 이용한 Theme적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import { useState } from "react";
+import styled from "styled-components";
 import Circle from "../StudyHistory/Circle";
 
 function App() {
   const [value, setValue] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const{currentTarget:{value},} = event;
+    const {
+      currentTarget: { value },
+    } = event;
     setValue(value);
   };
-  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log("Hello", value);  
-  }
+    console.log("Hello", value);
+  };
+
+  const ThemeTs = styled.div`
+    background-color: ${(props) => props.theme.bgColor};
+  `;
+  const Ts = styled.div`
+    color: ${(props) => props.theme.textColor};
+  `;
+
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input value={value}
-              onChange={onChange}
-              type="text"
-              placeholder="username" />
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
         <button>Log in</button>
       </form>
+
+      <ThemeTs>
+        <Ts>Protected</Ts>
+      </ThemeTs>
     </div>
   );
 }
