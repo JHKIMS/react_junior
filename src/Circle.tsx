73bb3333e9b2263c4e6751585fba81ebeea8dfc1
JHKIMS <wjdgns5131@gmail.com>
2023-01-14 11:57:38 +0900

    ReactJunior: Optional Props사용

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,23 +1,32 @@
 import styled from "styled-components"
 
 // interface는 object를 설명해주는 것이다.(Object가 어떤식으로 보일지 설명)
-interface ContainerProps {
+interface ContainerProps{
     bgColor: string;
+    borderColor : string; //  물음표(?) 붙여서 필수가 아닌 선택사항이 된다.
 }
+
 const Container = styled.div<ContainerProps>`
     width: 200px;
     height: 200px;
     background-color: ${(props)=>props.bgColor};
     border-radius: 100px;
+    border: 1px solid ${(props) => props.borderColor};
 `
 
 interface CircleProps{
     bgColor: string;
+    borderColor? : string; //  물음표(?) 붙여서 필수가 아닌 선택사항이 된다.
+    testText?: string;
 }
 
-function Circle({bgColor}: CircleProps){ // bgColor의 타입은 CircleProps의 Object이다. 
+function Circle({bgColor, borderColor, testText="Default testText Set"}: CircleProps){ // bgColor의 타입은 CircleProps의 Object이다. 
             // == (props: CircleProps) 
-    return <Container bgColor={bgColor} />
+    return (
+        <Container bgColor={bgColor} borderColor={borderColor ?? "blue"}>
+            {testText}
+        </Container>
+    );
 }
 
 
